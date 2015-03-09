8616a8383f69af67bdc1839e3f4979ee7542218e
max <maximapr@rambler.ru>
2015-03-09 19:42:34 +0200

    add calculator

---
+++ src/main/java/com/work/apiSide/NewApp.java
@@ -0,0 +1,7 @@
+package com.work.apiSide;
+
+/**
+ * Created by user on 26.02.2015.
+ */
+public class NewApp {
+}
---
+++ src/main/java/com/work/skillsup/Second.java
@@ -0,0 +1,7 @@
+package com.work.skillsup;
+
+/**
+ * Created by user on 26.02.2015.
+ */
+public class Second {
+}
