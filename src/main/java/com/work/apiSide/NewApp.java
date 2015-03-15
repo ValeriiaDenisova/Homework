57ae536126627aa288dc59bee0329b52d28a6d8c
max <dmytrijevavi@gmail.com>
2015-03-15 22:35:32 +0200

    Removed unused files

--- src/main/java/com/work/apiSide/NewApp.java
+++
@@ -1,7 +0,0 @@
-package com.work.apiSide;
-
-/**
- * Created by user on 26.02.2015.
- */
-public class NewApp {
-}
--- src/main/java/com/work/skillsup/First.java
+++
@@ -1,7 +0,0 @@
-package com.work.skillsup;
-
-/**
- * Created by user on 26.02.2015.
- */
-public class First {
-}
