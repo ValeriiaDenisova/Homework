c9c034d7371bbe46fc97a30f40e8b434d68bbf6f
maruska-lera <dmytrijevavi@gmail.com>
2015-04-21 12:19:03 +0300

    Test

--- YandexAutoTest/src/test/java/YandexTestPage.java
+++ YandexAutoTest/src/test/java/YandexTestPage.java
@@ -1,7 +1,13 @@
+import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import static org.openqa.selenium.support.PageFactory.initElements;
 
 /**
  * Created by v.dmitrieva on 21.04.15.
@@ -9,16 +15,34 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class YandexTestPage {
     private WebDriver driver;
 
+    @BeforeClass
+    public static void initPathese(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Desktop\\ranorextest\\drivers\\chromedriver.exe");
+    }
+
     @Before
-    public void setUp(){
+    public void setUp() {
         driver = new FirefoxDriver();
+        //driver = new ChromeDriver();
+        //driver = new HtmlUnitDriver();
         driver.get("http://yandex.ua");
     }
 
     @Test
     public void testSearch(){
-        HomePage home = new HomePage(driver);
-        RezultPage rezult = home.search("war and peace");
+        HomePage home = initElements(driver, HomePage.class);
+        //driver.get("http://yandex.ua");
+        home.search("war and peace");
+    }
+
+    /*@Test
+    public void testMarketSearch(){
+
+    }*/
+
+    @After
+    public void tearDown(){
+        driver.close();
     }
 
 
