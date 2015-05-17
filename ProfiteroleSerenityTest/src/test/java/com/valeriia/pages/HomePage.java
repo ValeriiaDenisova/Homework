074041be8ee4dd82072aefcab1bc4a66bedceee0
maruska-lera <dmytrijevavi@gmail.com>
2015-05-17 21:38:03 +0300

    Test

---
+++ ProfiteroleSerenityTest/src/test/java/com/valeriia/pages/HomePage.java
@@ -0,0 +1,230 @@
+package com.valeriia.pages;
+
+import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.pages.PageObject;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+/**
+ * Created by v.dmitrieva on 07.05.15.
+ */
+public class HomePage extends PageObject {
+
+ //   WebDriver driver;
+
+    /* public HomePage(WebDriver driver, WebDriver driver1) {
+        super(driver);
+    }
+*/
+    @FindBy(xpath = "//a[@href='/registration']")
+    WebElement registration;
+
+    @FindBy(id = "login")
+    WebElement fieldLogin;
+
+    @FindBy(id = "password")
+    WebElement fieldPassword;
+
+    @FindBy(id = "confirmPassword")
+    WebElement fieldConfirmPassword;
+
+    @FindBy(id = "email")
+    WebElement fieldEmail;
+
+    @FindBy(id = "datepicker")
+    WebElement fieldDatepicker;
+
+    @FindBy(id = "maleOrFemale1")
+    WebElement maleOrFemale1;
+
+    @FindBy(id = "maleOrFemale0")
+    WebElement maleOrFemale0;
+
+    @FindBy(id = "iAmAgree")
+    WebElement iAmAgree;
+
+    @FindBy(css = "a[target='_blank']")
+    WebElement refIAmAgree;
+
+    @FindBy(xpath = "//button[@class='btn']")
+    WebElement signUp;
+
+    @FindBy(id = "passwordError")
+    WebElement textPasswordError;
+
+    @FindBy(xpath = "//a[@href='/login']")
+    WebElement entry;
+
+    @FindBy(id = "j_username")
+    WebElement entryLogin;
+
+    @FindBy(id = "j_password")
+    WebElement entryPassword;
+
+    @FindBy(xpath = "//button[@class='btn']")
+    WebElement entryButton;
+
+    @FindBy(xpath = "//div[@class='alert alert-success']")
+    WebElement registrationSuccessful;
+
+    @FindBy(xpath = "//a[@href='/j_spring_security_logout']")
+    WebElement exit;
+
+    @FindBy(xpath = "//a[@href='/profile']")
+    WebElement profile;
+
+    @FindBy(xpath = "//button[@class='btn']")
+    WebElement profileButton;
+
+    @FindBy(xpath = "//option[@value='Франция']")
+    WebElement country;
+
+    @FindBy(id = "changePassword")
+    WebElement fieldChangePassword;
+
+    @FindBy(xpath = "//a[contains(text(),'Создать меню')]")
+    WebElement createMenu;
+
+    @FindBy(xpath = "//a[@href='/menu']")
+    WebElement createMenuForDay;
+
+    @FindBy(xpath = "//button[@value='#breakfast']")
+    WebElement breakfast;
+
+    @FindBy(xpath = "//img[@alt='Украинская']")
+    WebElement ukraineKitchen;
+
+    @FindBy(xpath = ".//*[@id='cuisine']/div[4]/div[2]/span/img")
+    WebElement firstMealSnacks;
+
+    @FindBy(id = "breakfast")
+    WebElement youBreakfast;
+
+    @FindBy(xpath = "//input[@class='search-query']")
+    WebElement searchField;
+
+    @FindBy(xpath = "//a[contains(text(),'Рецепты')]")
+    WebElement recipe;
+
+    @FindBy(xpath = "//a[contains(text(),'Все рецепты')]")
+    WebElement allRecipe;
+
+    @FindBy(xpath = "//img[@alt='Японская']")
+    WebElement japaneseFood;
+
+    @FindBy(xpath = "//a[contains(text(),'Закуски')]")
+    WebElement snacks;
+
+    @FindBy(xpath = "//div[contains(text(),'Пирожки с луком')]")
+    WebElement pattiesWithOnions;
+
+    public void get_main_page(String url) {
+        getDriver().get(url);
+    }
+
+    public void registration(){
+        registration.click();
+    }
+
+    public void fieldLogin(String login){
+        fieldLogin.sendKeys(login);
+    }
+
+    public void fieldPassword(String password){
+        fieldPassword.sendKeys(password);
+    }
+
+    public void fieldConfirmPassword(String confirmPassword){
+        fieldConfirmPassword.sendKeys(confirmPassword);
+    }
+
+    public void fieldEmail(String email){
+        fieldEmail.sendKeys(email);
+    }
+
+    public void iAmAgree(){
+        iAmAgree.click();
+    }
+
+    public void signUp(){
+        signUp.click();
+    }
+
+    public void entry(){
+        entry.click();
+    }
+
+    public void entryLogin(String enter_login){
+        entryLogin.sendKeys(enter_login);
+    }
+
+    public void entryPassword(String enter_password){
+        entryPassword.sendKeys(enter_password);
+    }
+
+    public void entryButton(){
+        entryButton.click();
+    }
+
+    public void profile(){
+        profile.click();
+    }
+
+    public void fieldChangePassword(String enter_new_password){
+        fieldChangePassword.sendKeys(enter_new_password);
+    }
+
+    public void fieldDatepicker(String datepicker){
+        fieldDatepicker.sendKeys(datepicker);
+    }
+
+    public void country(){
+        country.click();
+    }
+
+    public void createMenu(){
+        createMenu.click();
+    }
+
+    public void createMenuForDay(){
+        createMenuForDay.click();
+    }
+
+    public void breakfast(){
+        breakfast.click();
+    }
+
+    public void ukraineKitchen(){
+        ukraineKitchen.click();
+    }
+
+    public void dragAndDrop() {
+        Actions actions = new Actions(getDriver());
+        actions.dragAndDrop(firstMealSnacks, youBreakfast).build().perform();
+    }
+
+    public void searchField(String search_word){
+        searchField.sendKeys(search_word);
+    }
+
+    public void recipe(){
+        recipe.click();
+    }
+
+    public void allRecipe(){
+        allRecipe.click();
+    }
+
+    public void japaneseFood(){
+        japaneseFood.click();
+    }
+
+    public void snacks(){
+        snacks.click();
+    }
+
+    public void pattiesWithOnions(){
+        pattiesWithOnions.click();
+    }
+
+}
---
+++ ProfiteroleSerenityTest/src/test/java/com/valeriia/steps/EndUserSteps.java
@@ -0,0 +1,168 @@
+package com.valeriia.steps;
+
+import com.valeriia.pages.HomePage;
+import net.thucydides.core.annotations.Step;
+import net.thucydides.core.steps.ScenarioSteps;
+
+import static com.thoughtworks.selenium.SeleneseTestBase.assertTrue;
+
+/**
+ * Created by v.dmitrieva on 07.05.15.
+ */
+public class EndUserSteps extends ScenarioSteps {
+
+    public HomePage getPage()
+    {
+        return  getPages().currentPageAt(HomePage.class);
+    }
+
+    @Step
+    public void get_main_page(String url) {
+        getPage().get_main_page(url);
+        assertTrue(getPage().containsText("Добро пожаловать на сайт"));
+    }
+
+    @Step
+    public void click_on_registration_link() {
+        getPage().registration();
+    }
+
+    @Step
+    public void enter_login(String login) {
+        getPage().fieldLogin(login);
+    }
+
+    @Step
+    public void enter_password(String password) {
+        getPage().fieldPassword(password);
+    }
+
+    @Step
+    public void enter_confirmPassword(String confirmPassword) {
+        getPage().fieldConfirmPassword(confirmPassword);
+    }
+
+    @Step
+    public void enter_email(String email) {
+        getPage().fieldEmail(email);
+    }
+
+    @Step
+    public void click_i_am_agree() {
+        getPage().iAmAgree();
+    }
+
+    @Step
+    public void click_button_sign_up() {
+        getPage().signUp();
+        assertTrue(getPage().containsText("Пользователь с таким логином уже существует."));
+    }
+
+    @Step
+    public void click_on_entry_link() {
+        getPage().entry();
+    }
+
+    @Step
+    public void enter_login_on_the_entry_page(String enter_login) {
+        getPage().entryLogin(enter_login);
+    }
+
+    @Step
+    public void enter_password_on_the_entry_page(String enter_password) {
+        getPage().entryPassword(enter_password);
+    }
+
+    @Step
+    public void click_on_entry_button() {
+        getPage().entryButton();
+    }
+
+    @Step
+    public void click_on_profile_link() {
+        getPage().profile();
+    }
+
+    @Step
+    public void click_on_profile_button() {
+        getPage().entryButton();
+    }
+
+    @Step
+    public void enter_new_password(String enter_new_password) {
+        getPage().fieldChangePassword(enter_new_password);
+    }
+
+    @Step
+    public void enter_new_confirmPassword(String enter_new_confirmPassword) {
+        getPage().fieldConfirmPassword(enter_new_confirmPassword);
+    }
+
+    @Step
+    public void enter_datepicker(String datepicker) {
+        getPage().fieldDatepicker(datepicker);
+    }
+
+    @Step
+    public void select_country_France() {
+        getPage().country();
+    }
+
+    @Step
+    public void click_on_create_menu() {
+        getPage().createMenu();
+    }
+
+    @Step
+    public void click_on_create_menu_for_day() {
+        getPage().createMenuForDay();
+    }
+
+    @Step
+    public void click_on_breakfast() {
+        getPage().breakfast();
+    }
+
+    @Step
+    public void click_on_ukraine_kitchen() {
+        getPage().ukraineKitchen();
+    }
+
+    @Step
+    public void drag_and_drop_first_meal_snacks_on_you_breakfast_field() {
+        getPage().dragAndDrop();
+    }
+
+
+    @Step
+    public void enter_borsh_in_search_field(String search_word) {
+        getPage().searchField(search_word);
+        assertTrue(getPage().containsText("Борщ"));
+    }
+
+    @Step
+    public void click_on_recipe() {
+        getPage().recipe();
+    }
+
+    @Step
+    public void click_on_all_recipe() {
+        getPage().allRecipe();
+    }
+
+    @Step
+    public void click_on_japanese_food() {
+        getPage().japaneseFood();
+    }
+
+    @Step
+    public void click_on_snacks() {
+        getPage().snacks();
+    }
+
+    @Step
+    public void click_on_patties_with_onions() {
+        getPage().pattiesWithOnions();
+        assertTrue(getPage().containsText("Пирожки с луком"));
+    }
+}
